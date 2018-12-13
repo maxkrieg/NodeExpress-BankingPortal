3b09b23abd5aceb1f6dbdb60c5b4390aeca918f5
Max Krieg <maxwell.krieg@gmail.com>
2018-12-13 08:51:57 -0500

    Add user data to app

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const accountData = fs.readFileSync('./src/json/accounts.json')
-const account = JSON.parse(accountData)
+const accountData = fs.readFileSync('./src/json/accounts.json', 'utf-8')
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync('./src/json/users.json', 'utf-8')
+const users = JSON.parse(userData)
+
 app.get('/', (req, res) => res.render('index', { title: 'Index' }))
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') })
