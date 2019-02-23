119b61ddc4728fc296c5f77d07c9dc2f4fcc9752
Max Krieg <maxwell.krieg@gmail.com>
2019-02-23 15:02:33 -0500

    Add more routes and views

--- src/app.js
+++ src/app.js
@@ -18,4 +18,13 @@ app.get('/', (req, res) => res.render('index', {
   title: 'Account Summary',
   accounts: accounts
 }))
+app.get('/savings', (req, res) => res.render('account', {
+  account: accounts.savings
+}))
+app.get('/checking', (req, res) => res.render('account', {
+  account: accounts.checking
+}))
+app.get('/credit', (req, res) => res.render('account', {
+  account: accounts.credit
+}))
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') })
